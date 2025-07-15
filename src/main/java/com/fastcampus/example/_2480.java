6118792a7eae403748d972d0f9e0f48c1a046507
노희현 <81168262+Heehyun9@users.noreply.github.com>
2025-07-16 07:51:10 +0900

    feat: 백준 2480번 풀이

--- src/main/java/com/fastcampus/example/_2480.java
+++ src/main/java/com/fastcampus/example/_2480.java
@@ -1,8 +1,8 @@
-package com.fastcampus;
+package com.fastcampus.example;
 
 import java.util.Scanner;
 
-public class Main {
+public class _2480 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
