e5ba7903c361762dfb7f3fec5c7acad0f129d312
노희현 <81168262+Heehyun9@users.noreply.github.com>
2025-07-17 05:50:00 +0900

    feat: 1008번 문제

---
+++ src/main/java/com/fastcampus/example/_1008.java
@@ -0,0 +1,21 @@
+package com.fastcampus.example;
+
+import java.util.Scanner;
+
+public class _1008 {
+    public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    String string_a = sc.next();
+    String string_b = sc.next();
+
+    int a = Integer.parseInt(string_a);
+    int b = Integer.parseInt(string_b);
+    System.out.print(a/(double)b);
+    sc.close();
+
+    }
+}
+
+
+
