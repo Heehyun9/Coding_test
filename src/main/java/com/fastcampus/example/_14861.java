5ace15a5168ed36a26aaa704808d4b3d140def9f
노희현 <81168262+Heehyun9@users.noreply.github.com>
2025-07-20 13:35:20 +0900

    feat: 문제 14861번

---
+++ src/main/java/com/fastcampus/example/_14861.java
@@ -0,0 +1,26 @@
+package com.fastcampus.example;
+import java.util.*;
+
+import java.util.Scanner;
+
+
+public class _14861 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        if (x>0 & y>0){
+            System.out.println(1);
+        }
+        else if(x<0 & y>0){
+            System.out.println(2);
+        }
+        else if(x<0 & y<0){
+            System.out.println(3);
+        }
+        else {
+            System.out.println(4);}
+        sc.close();
+    }
+}
